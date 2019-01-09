b30bbefc2ca24412da58e2482107e8d779d5d508
jaredanderton <jared@andertondev.com>
2019-01-08 23:27:34 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/jaredanderton/Swift-Kuery.git", from: "3.0.1"),
+        .package(url: "https://github.com/jaredanderton/Swift-Kuery.git", from: "3.0.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
